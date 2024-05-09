74ea69075cb58c5de6b2e017a568645ded354e36
mayssam73 <mayssamkalajo@gmail.com>
2024-05-08 21:59:52 -0500

    Added flexbox skeleton

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,19 @@ import { Roboto } from 'next/font/google';
 export default function Home() {
   return (
     <main>
-      <div>Google Calendar</div>
+      <div className="flex">
+        <div className="flex-auto w-[25%]">
+          <div>Google Calendar</div>
+        </div>
+        <div className="h-screen w-px bg-gray-200 border-0 darkBg-gray-700"></div>
+        <div className="flex-auto w-[75%]">
+          <div>Hello</div>
+          <hr className="h-px my-[10%] bg-gray-200 border-0 darkBg-gray-700"></hr>
+          <div className="flex-auto w-14">
+            <div>Hi</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
